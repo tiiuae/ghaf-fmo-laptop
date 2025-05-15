a0ab8d893a0a20411e9f020a5a000c05c1342c80
Janne Sangi <janne.sangi@tii.ae>
2025-05-15 14:41:17 +0400

    script for settings network
    Signed-off-by: Janne Sangi <janne.sangi@tii.ae>

--- packages/flake-module.nix
+++ packages/flake-module.nix
@@ -6,6 +6,7 @@
     fmo-build-helper = final.callPackage ./fmo-build-helper/default.nix { };
     fmo-onboarding = final.callPackage ./fmo-onboarding/default.nix { };
     fmo-offboarding = final.callPackage ./fmo-offboarding/default.nix { };
+    fmo-set-netw-con = final.callPackage ./fmo-set-netw-con/default.nix { };
     onboarding-agent = inputs.onboarding-agent.packages.${final.stdenv.hostPlatform.system}.default;
   };
 }
