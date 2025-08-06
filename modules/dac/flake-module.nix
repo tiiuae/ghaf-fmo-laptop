70ff1e195325c5eea5731c4ebdb683bc5bf5ddc4
solita-cristian <cristiannicolae.lupascu@solita.fi>
2025-08-06 11:52:04 +0300

    feat(dac): add DAC agent
    Signed-off-by: solita-cristian <cristiannicolae.lupascu@solita.fi>

---
+++ modules/dac/flake-module.nix
@@ -0,0 +1,10 @@
+# Copyright 2022-2025 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  flake.nixosModules = {
+    dac-services.imports = [
+      ./dac-nats-server
+      ./dac-agent
+    ];
+  };
+}
