f3c2377482adf1a572b6f3f4a10173a2b6e6345c
solita-cristian <cristiannicolae.lupascu@solita.fi>
2025-11-03 12:03:23 +0200

    bump: enrol-mc
    Signed-off-by: solita-cristian <cristiannicolae.lupascu@solita.fi>

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
     };
 
     kms-enrolment = {
-      url = "git+ssh://git@github.com/tiiuae/enroll-mc?ref=feature/request-timeout";
+      url = "git+ssh://git@github.com/tiiuae/enroll-mc";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "ghaf/flake-utils";
