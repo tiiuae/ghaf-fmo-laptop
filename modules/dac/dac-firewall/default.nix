a9637d43bbf707e6d3598ee233dcaa0112977d33
solita-cristian <cristiannicolae.lupascu@solita.fi>
2025-11-11 14:40:09 +0200

    fix: dac usb network conf
    Signed-off-by: solita-cristian <cristiannicolae.lupascu@solita.fi>

--- modules/dac/dac-firewall/default.nix
+++ modules/dac/dac-firewall/default.nix
@@ -1,12 +1,13 @@
 # Copyright 2022-2025 TII (SSRC) and the Ghaf contributors
 # SPDX-License-Identifier: Apache-2.0
 {
+  pkgs,
   lib,
   config,
   ...
 }:
 let
-  cfg = config.services.dac-firewall;
+  cfg = config.services.dac-usb-network-configuration;
 
   inherit (lib)
     mkIf
@@ -25,27 +26,19 @@ let
       gwip,
     }:
     ''
-      /run/current-system/sw/bin/ip ad ${action} ${ip} dev "$1" 2> /dev/null || true
-      logger -t fmo-fw "${action} IP ${ip} to $1"
+      ${pkgs.iproute2}/bin/ip ad ${action} ${ip} dev "$IFACE" 2> /dev/null || true
+      logger -t dac-usb-network-configuration "${action} IP ${ip} to $IFACE"
 
-      /run/current-system/sw/bin/ip link set dev "$1" ${state} 2> /dev/null || true
-      logger -t fmo-fw "$1 set to ${state}"
+      ${pkgs.iproute2}/bin/ip link set dev "$IFACE" ${state} 2> /dev/null || true
+      logger -t dac-usb-network-configuration "$IFACE set to ${state}"
 
-      /run/current-system/sw/bin/ip route ${action} ${kmsip} via ${gwip} dev "$1" 2> /dev/null || true
-      logger -t fmo-fw "${action}: $1 -> ${kmsip} via ${gwip}"
+      ${pkgs.iproute2}/bin/ip route ${action} ${kmsip} via ${gwip} dev "$IFACE" 2> /dev/null || true
+      logger -t dac-usb-network-configuration "${action}: $IFACE -> ${kmsip} via ${gwip}"
     '';
 in
 {
-  options.services.dac-firewall = {
-    enable = mkEnableOption "dac-firewall";
-
-    mtu = mkOption {
-      type = types.nullOr types.int;
-      default = null;
-      description = ''
-        MTU for the external network interfaces.
-      '';
-    };
+  options.services.dac-usb-network-configuration = {
+    enable = mkEnableOption "dac-usb-network-configuration";
 
     ip = mkOption {
       type = types.str;
@@ -74,19 +67,12 @@ in
 
   config = mkIf cfg.enable {
 
-    ghaf.firewall = {
-      enable = true;
-    };
-
-    # Set MTU for external USB network devices
-    services.udev.extraRules = optionalString (cfg.mtu != null) ''
-      ACTION=="add", SUBSYSTEM=="net", KERNEL=="usb*", ATTR{mtu}="${toString cfg.mtu}"
-    '';
-
-    # Set MTU and port forwarding rules for external network interfaces
-    environment.etc."NetworkManager/dispatcher.d/99-fmo-kms" = {
+    # Set network for USB ethernet adapters
+    environment.etc."NetworkManager/dispatcher.d/99-dac-usb-network-configuration" = {
       text = ''
         #!/bin/sh
+        IFACE="$1"
+        STATUS="$2"
 
         add_rules(){
             ${mkFirewallRules {
@@ -101,44 +87,25 @@ in
         remove_rules(){
             ${mkFirewallRules {
               inherit (cfg) ip;
-              action = "add";
-              state = "up";
+              action = "del";
+              state = "down";
               inherit (cfg) kmsip;
               inherit (cfg) gwip;
             }}
         }
 
-        echo "Detecting USB Ethernet adapters..."
+        # Check if backed by USB
+        if readlink -f /sys/class/net/$IFACE | grep -q '/usb'; then
+            logger -t dac-usb-network-configuration "$IFACE is a USB Ethernet adapter"
 
-        for iface in /sys/class/net/*; do
-            iface=$(basename "$iface")
-            
-            # Skip loopback and common virtual interfaces
-            [[ "$iface" == "lo" ]] && continue
-            [[ "$iface" =~ tun|vbox|docker|veth|br-|virbr ]] && continue
-
-            # Only Ethernet type (1)
-            type=$(cat /sys/class/net/$iface/type)
-            [[ "$type" != "1" ]] && continue
-
-            # Check if backed by USB
-            if readlink -f /sys/class/net/$iface | grep -q '/usb'; then
-                log "$iface is a USB Ethernet adapter"
-
-                # TODO: Set rule if status is UP, remove if DOWN
-                status=$(cat /sys/class/net/$iface/operstate)
-                if [ "$status" = "up" ]; then
-                    log "Interface $iface is up, applying rules"
-                    add_rules "$iface"
-
-                    # Set MTU for the interface
-                    ${optionalString (cfg.mtu != null) ''ip link set dev "$iface" mtu ${toString cfg.mtu}''}
-                else
-                    log "Interface $iface is down, removing rules"
-                    remove_rules "$iface"
-                fi
+            if [ "$STATUS" = "up" ]; then
+                logger -t dac-usb-network-configuration "Interface $IFACE is up, applying rules"
+                add_rules
+            else
+                logger -t dac-usb-network-configuration "Interface $IFACE is down, removing rules"
+                remove_rules
             fi
-        done
+        fi
       '';
       mode = "0700";
     };
